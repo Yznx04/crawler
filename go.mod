ce82c0a440443d3b16e9727970120373781a015c
AidenYue <2687927133@qq.com>
2025-09-02 15:20:47 +0800

    第19课程代码

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/yznx04/crawler
 
 go 1.25
+
+require (
+	github.com/antchfx/xpath v1.3.5 // indirect
+	golang.org/x/net v0.43.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/antchfx/htmlquery v1.3.4
+	golang.org/x/text v0.28.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+)
--- main.go
+++ main.go
@@ -1,32 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
-func main() {
-	url := "http://www.thepaper.cn"
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Printf("fetch error:%v\n", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+
+}
+
+// Fetch tag v0.0.4
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("fetch url, error:", err)
-		return
+		panic(err)
 	}
+
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			return
+			panic(err)
 		}
 	}(resp.Body)
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("status code", resp.StatusCode)
-		return
+		fmt.Printf("Error status code: %d", resp.StatusCode)
 	}
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+var headerRe = regexp.MustCompile(`<div class="new_li"[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`)
+
+// tag v0.0.3
+func main() {
+	url := "http://www.thepaper.cn"
+	body, err := Fetch(url)
+
 	if err != nil {
 		fmt.Println("read body, error:", err)
 	}
-	fmt.Println(string(body))
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	doc.Find("a.index_inherit__A1ImK").Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		fmt.Println("title:", title)
+	})
+
 }
