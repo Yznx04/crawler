e288491a943a599c1594de9bb58c18b93b7aa66d
AidenYue <2687927133@qq.com>
2025-09-13 22:37:09 +0800

    21

---
+++ collect/collect.go
@@ -0,0 +1,73 @@
+package collect
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+)
+
+type Fetch interface {
+	Get(url string) ([]byte, error)
+}
+
+type BaseFetch struct{}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Printf("fetch error:%v\n", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+
+}
+
+func (f *BaseFetch) Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code: %d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+// BrowserFetch 支持设置请求头的请求方式
+type BrowserFetch struct{}
+
+func (b *BrowserFetch) Get(url string) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get url failed:%v", err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
--- main.go
+++ main.go
@@ -1,60 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io"
-	"net/http"
-	"regexp"
 
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
+	"github.com/yznx04/crawler/collect"
 )
 
-func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		fmt.Printf("fetch error:%v\n", err)
-		return unicode.UTF8
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-
-}
-
 // Fetch tag v0.0.4
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code: %d", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DetermineEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return io.ReadAll(utf8Reader)
-}
-
-var headerRe = regexp.MustCompile(`<div class="new_li"[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`)
 
 // tag v0.0.3
 func main() {
 	url := "http://www.thepaper.cn"
-	body, err := Fetch(url)
+	baseFetch := collect.BaseFetch{}
+	body, err := baseFetch.Get(url)
 
 	if err != nil {
 		fmt.Println("read body, error:", err)
